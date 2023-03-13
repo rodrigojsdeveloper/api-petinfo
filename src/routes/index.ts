2b85ed6761e05cab706b1e3c6cfa927bc68e7e77
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-03-13 20:15:43 -0300

    feat: post creation route created

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { usersRoutes } from "./users.routes";
 import { loginRoutes } from "./login.routes";
+import { postRoutes } from "./post.routes";
 import { Express } from "express";
 
 const appRoutes = (app: Express) => {
   app.use("/users", usersRoutes());
   app.use("/signin", loginRoutes());
+  app.use("/posts", postRoutes());
 };
 
 export { appRoutes };
---
+++ src/routes/post.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+import { PostController } from "../controllers/post.controller";
+
+const routes = Router();
+
+const postRoutes = () => {
+  routes.post("/", new PostController().create);
+
+  return routes;
+};
+
+export { postRoutes };
