da7179321040b8a70dfb1cce31e42a3925d0d589
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-03-13 19:39:17 -0300

    feat: login route created

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { usersRoutes } from "./users.routes";
+import { loginRoutes } from "./login.routes";
 import { Express } from "express";
 
 const appRoutes = (app: Express) => {
   app.use("/users", usersRoutes());
+  app.use("/signin", loginRoutes());
 };
 
 export { appRoutes };
