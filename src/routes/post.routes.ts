7c16a125177ab1a835d9ee482e0351e47b30c23d
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-03-13 21:07:33 -0300

    feat: token middleware to check if there is a token created and configured in routes

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -2,18 +2,20 @@ import { Router } from "express";
 
 import { PostController } from "../controllers/post.controller";
 
+import { tokenMiddleware } from "../middlewares/token.middleware";
+
 const routes = Router();
 
 const postRoutes = () => {
-  routes.post("", new PostController().create);
+  routes.post("", tokenMiddleware, new PostController().create);
 
-  routes.get("", new PostController().list);
+  routes.get("", tokenMiddleware, new PostController().list);
 
-  routes.patch("/:id", new PostController().update);
+  routes.patch("/:id", tokenMiddleware, new PostController().update);
 
-  routes.delete("/:id", new PostController().delete);
+  routes.delete("/:id", tokenMiddleware, new PostController().delete);
 
-  routes.get("/:id", new PostController().view);
+  routes.get("/:id", tokenMiddleware, new PostController().view);
 
   return routes;
 };
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 
 import { UserController } from "../controllers/user.controller";
 
+import { tokenMiddleware } from "../middlewares/token.middleware";
+
 const routes = Router();
 
 const usersRoutes = () => {
   routes.post("/signup", new UserController().create);
 
-  routes.get("/profile", new UserController().create);
+  routes.get("/profile", tokenMiddleware, new UserController().create);
 
   return routes;
 };
